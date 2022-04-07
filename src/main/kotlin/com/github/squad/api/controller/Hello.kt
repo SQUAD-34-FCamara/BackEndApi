55ca4949d00be923c48058088a09ebeabb8531c6
woggo <paulotimbu653@gmail.com>
2022-04-07 14:36:42 -0300

    configurações

--- src/main/kotlin/com/github/squad/api/controller/Hello.kt
+++ src/main/kotlin/com/github/squad/api/controller/Hello.kt
@@ -1,4 +1,14 @@
 package com.github.squad.api.controller
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/hi")
 class Hello {
+    @GetMapping()
+    fun hi(): String {
+        return "Hello World!"
+    }
 }
