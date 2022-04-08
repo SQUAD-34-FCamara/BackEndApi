1367ee7dafd2027b4996b1f7f1f86cc5b06b0619
woggo <paulotimbu653@gmail.com>
2022-04-08 15:55:15 -0300

    adicionando algumas validações na request do mentor

--- src/main/kotlin/com/github/squad/api/dto/request/MentorRequest.kt
+++ src/main/kotlin/com/github/squad/api/dto/request/MentorRequest.kt
@@ -1,14 +1,20 @@
 package com.github.squad.api.dto.request
 
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotEmpty
+
 
 data class MentorRequest (
-
+    @field:NotEmpty(message = "O Nome completo deve ser informado")
     var nomeCompleto: String,
+    @field:NotEmpty(message = "O Nome deve ser informado")
     var nome: String,
+    @field:Email(message = "O Email deve ser informado")
     var email: String,
+    @field:NotEmpty(message = "A senha deve ser informada")
     var senha: String,
     var especialidades: List<String>?,
+    @field:NotEmpty(message = "O link do linkedin deve ser informado")
     var linkLinkedin: String,
     var linkMeet: String
-
 )
